d017f83baaa2f0ad71fb3f20c6b187b01ffe8d26
NodeMixaholic <9904667+NodeMixaholic@users.noreply.github.com>
2023-11-14 19:09:34 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 
 let gun = Gun(['http://localhost:8765/gun', 'https://gun-manhattan.herokuapp.com/gun']);
-const coredb = gun.get(`mayaspace`)
-const postsDB = coredb.get('posts')
+let coredb = gun.get(`mayaspace`)
+let postsDB = coredb.get('posts')
 let usrname;
 function showMainPage() {
     body.innerHTML = `<img class="logo" src="maya.jpg">

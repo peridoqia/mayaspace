67542a47c50d5bb52edd24b22aa2ac6bf66688fb
NodeMixaholic <9904667+NodeMixaholic@users.noreply.github.com>
2023-11-16 14:02:28 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,91 +1,67 @@
-
 let gun = Gun(['http://localhost:8765/gun', 'https://gun-manhattan.herokuapp.com/gun']);
-let coredb = gun.get(`mayaspace`)
-let postsDB = coredb.get('posts')
+let coredb = gun.get(`mayaspace`);
+let postsDB = coredb.get('posts');
 let usrname;
-let postContainer;
 
 function showMainPage() {
     body.innerHTML = `<img class="logo" src="maya.jpg">
-    <h1>MayaSpace</h1><hr><br>
-    <div class="container" id="poster">
-    <input type="text" placeholder="Type a post here..." name="post" id="post" required>
-    <button onclick="post()">Post</button>
-    </div>
-    <div class="container" id="postContainer">
-    <hr>
-    Copyright Samuel Lord. All rights reserved.
-    </div>
+        <h1>MayaSpace</h1><hr><br>
+        <div class="container" id="poster">
+            <input type="text" placeholder="Type a post here..." name="post" id="post" required>
+            <button onclick="post()">Post</button>
+        </div>
+        <div class="container" id="postContainer">
+            <hr>
+            Copyright Samuel Lord. All rights reserved.
+        </div>
     `;
 
-    postContainer = document.getElementById("postContainer");
+    let postContainer = document.getElementById("postContainer");
 
-    // Use on() instead of once() to continuously listen for changes
+    // Use on() to continuously listen for changes
     postsDB.on((data, key) => {
         addPost(data);
     });
 }
 
-async function sha256HexPromise(data) {
-  const msgUint8 = new TextEncoder().encode(data); // encode as (utf-8) Uint8Array
-  const hashBuffer = await crypto.subtle.digest("SHA-256", msgUint8); // hash the message
-  const hashArray = Array.from(new Uint8Array(hashBuffer)); // convert buffer to byte array
-  const hashHex = hashArray
-    .map((b) => b.toString(16).padStart(2, "0"))
-    .join(""); // convert bytes to hex string
-  return hashHex;
-}
-
-function login() {
+function login() { 
     let password = document.getElementById("password").value;
+    let encryptedPassword = hex_sha256(password);
+    usrname = `${document.getElementById("uname").value}@${window.location.hostname}`;
+    let userRef = coredb.get('users').get(usrname);
 
-    // Perform the SHA-256 hashing asynchronously
-    sha256HexPromise(password)
-        .then(function (encryptedPassword) {
-            usrname = `${document.getElementById("uname").value}@${window.location.hostname}`;
-            return encryptedPassword;
-        })
-        .then(function (encryptedPassword) {
-            // Access the user reference and check the stored password
-            let userRef = coredb.get('users').get(usrname);
-            let storedPassword = coredb.get('users').get(usrname).get("passwd");
-
-            storedPassword.once((storedData) => {
-                if (storedData === undefined || storedData === null || storedData === "") {
-                    storedPassword.put(`${encryptedPassword}`);
-                    showMainPage();
-                    console.log("registering and logging in....");
-                } else if (storedData === encryptedPassword) {
-                    showMainPage();
-                    console.log("welcome back!");
-                } else {
-                    alert("Incorrect Password");
-                }
-            });
-        })
-        .catch(function (error) {
-            console.error('Error in sha256HexPromise:', error);
-            // Handle the error appropriately
-        });
+    userRef.once((data) => {
+        let storedPassword = data.encryptedPassword;
+        if (!storedPassword) {
+            userRef.put({ encryptedPassword });
+            showMainPage();
+            console.log("passwd");
+        } else if (storedPassword === encryptedPassword) {
+            showMainPage();
+        } else {
+            alert("Incorrect Password");
+        }
+    });
 }
 
-    function addPost(data) {
-          let postElement = document.createElement('div');
-          postElement.textContent = filterXSS(data);
-          postContainer.appendChild(postElement);
-          postContainer.appendChild(document.createElement('br'));
-    }
+function addPost(data) {
+    let postContainer = document.getElementById("postContainer");
 
-    function post() {
-        let postData = `[${usrname}]: ${filterXSS(document.getElementById("post").value)}`;
-        addPost(postData);
-        postsDB.put(postData);
-        console.log("USER: " + usrname);
-    }
+    let postElement = document.createElement('div');
+    postElement.textContent = filterXSS(data);
+    postContainer.appendChild(postElement);
+    postContainer.appendChild(document.createElement('br'));
+}
+
+function post() {
+    let postData = `[${usrname}]: ${filterXSS(document.getElementById("post").value)}`;
+    addPost(postData);
+    postsDB.put(postData);
+    console.log("USER: " + usrname);
+}
 
 function logout() {
     document.location.href = "index.html";
 }
-console.log("loaded")
-postsDB.on((data) => { addPost(data) });
 
+console.log("loaded");
